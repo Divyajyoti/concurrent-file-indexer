673bb69660af97da6e738815e5acf7b1daad80ee
divyajyoti <divyajyoti@hivepro.com>
2023-12-27 01:47:21 +0530

    added lucene dependency

--- src/main/java/com/fileindexer/Main.java
+++ src/main/java/com/fileindexer/Main.java
@@ -1,7 +1,13 @@
-package org.fileindexer;
+package com.fileindexer;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ST
+
     }
-}
+
+
+
+
+    }
